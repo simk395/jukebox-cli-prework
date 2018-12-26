c5405de412b784e2778f3aa2a850fca2e3ab1b9f
Simon Kong <simon.kong95@gmail.com>
2018-12-26 22:31:10 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -27,12 +27,10 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   response = gets.chomp
-  if songs.include?(response) || response == "1".."songs.size"
-    if response.class == String
-      puts "Playing #{response}"
-    elsif response.class == Fixnum
-      puts "Playing #{songs[response-1]}"
-    end
+  if songs.include?(response)
+    puts "Playing #{response}"
+  elsif response == "1".."songs.size"
+    puts "Playing #{songs[response.to_i -1]}"
   else
     puts "Invalid input, please try again"
   end#if
